c6fd1f1a4d79d321f672d3e8da60a3b52845df86
swarup <41253664+swarsutar99@users.noreply.github.com>
2022-12-21 11:59:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Basic Banking System</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body class="bg-dark">
+	<header>
+		<?php
+		include 'navbar.php';
+		?>
+	</header>
+	<main>
+		<div class="m-container">
+			<div>
+				<div class="m-container-item">
+					<h1 class="title">THE SPARKS BANK</h1>
+					<h2 class="tagline">For All Your Financial Needs</h2>
+				</div>
+				<div class="m-container-item">
+					<a href="customers.php">
+						<button type="button" class="btn btn-primary">View all customers</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer class="footer text-center py-2">
+		<p>© Project created by <a href="https://www.linkedin.com/in/hardikmaks17/">Hardik Makwana</a> under <a href="https://www.linkedin.com/company/the-sparks-foundation/">The Sparks Foundation</a> internship. <b>(#GripAugust21)</b></p>
+	</footer>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,10 @@
+<nav style="background-color:#000" class="navbar sticky-top navbar-expand-md navbar-dark">
+	<a class="navbar-brand" href="index.php">The Sparks Bank</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"><span class="navbar-toggler-icon"></span></button>
+	<div class="collapse navbar-collapse" id="collapsibleNavbar">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+			<li class="nav-item"><a class="nav-link" href="transactionhistory.php">Transaction History</a></li>
+		</ul>
+	</div>
+</nav>
